1d0b75738456620f84b82ead1eddd6d0dd066a30
Ben Jackman <ben@jackman.biz>
2015-01-06 23:58:42 -0600

    Support calls through nested vals.
    Signed-off-by: Ben Jackman <ben@jackman.biz>

---
+++ shared/test/scala/autowire/RelativeRoutingTests.scala
@@ -0,0 +1,69 @@
+package autowire
+
+import java.io.File
+import utest._
+import utest.ExecutionContext.RunNow
+import upickle._
+import acyclic.file
+
+
+object RelativeRoutingTests extends TestSuite {
+ import utest.PlatformShims.await
+
+  val goodHand = Seq("Black Lotus", "Mountain", "Fireball", "Channel")
+
+  val tests = TestSuite {
+    'example {
+      import upickle._
+
+      // shared API interface
+      trait MagicalApi {
+        def resetGame(): Seq[String]
+        def shuffle(x: Int, s: String, d : String = "Foo"): String
+        val table: TableApi
+        var avartable: TableApi = _
+      }
+
+      trait TableApi {
+        def getHand(playerName: String): Seq[String]
+      }
+      // server-side implementation, and router
+      class MyApiImpl(c: String) extends MagicalApi {
+        override def resetGame(): Seq[String] = ???
+        override val table: TableApi = new TableApi {
+          override def getHand(playerId: String): Seq[String] = {
+            if (playerId == "Ben") {
+              goodHand
+            } else {
+              Nil
+            }
+          }
+        }
+        override def shuffle(x: Int, s: String, d: String = "Foo"): String = x + ":" + s + ":" + d
+      }
+
+
+      trait UPickleSerializers extends Serializers[String, upickle.Reader, upickle.Writer] {
+        override def write[Result: Writer](r: Result) = upickle.write(r)
+        override def read[Result: Reader](p: String) = upickle.read[Result](p)
+      }
+
+      object UPickleServer extends autowire.Server[String, upickle.Reader, upickle.Writer] with UPickleSerializers
+      val x = new MyApiImpl("aa")
+      val router = UPickleServer.route[MagicalApi](x)
+
+      // client-side implementation, and call-site
+      object MyClient extends autowire.Client[String, upickle.Reader, upickle.Writer] with UPickleSerializers {
+        override def doCall(req: Request) = {
+          router(req)
+        }
+      }
+
+      val a = await(MyClient[MagicalApi].shuffle(3, "lol").call())
+      val b = await(MyClient[MagicalApi].table.getHand("Ben").call())
+
+      assert("3:lol:Foo" == a)
+      assert(goodHand == b)
+    }
+  }
+}
