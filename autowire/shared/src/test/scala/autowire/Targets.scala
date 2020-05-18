b1188d01e8192a07b4d3b2e2911186e0cc4098fd
Mathieu <mathieu.leclaire@iscpif.fr>
2020-05-18 22:59:34 +0200

    Fix tests

--- autowire/shared/src/test/scala/autowire/Targets.scala
+++ autowire/shared/src/test/scala/autowire/Targets.scala
@@ -1,24 +1,31 @@
 package autowire
-import acyclic.file
+
 import scala.concurrent.Future
 
-trait Api{
+trait Api {
   def multiply(x: Double, ys: Seq[Double]): String
+
   def add(x: Int, y: Int = 1 + 1, z: Int = 10): String
+
   def sloww(s: Seq[String]): Future[Seq[Int]]
- // def sum(p1: Point, p2: Point): Point
+
+  //def sum(p1: Point, p2: Point): Point
 }
 
-trait FakeApi{
+trait FakeApi {
   def omg(x: Int): Int
 }
 
-object Controller extends Api{
-  def multiply(x: Double, ys: Seq[Double]): String = x + ys.map("*"+_).mkString
+object Controller extends Api {
+  def multiply(x: Double, ys: Seq[Double]): String = x + ys.map("*" + _).mkString
+
   def add(x: Int, y: Int = 1 + 1, z: Int = 10): String = s"$x+$y+$z"
+
   def sloww(s: Seq[String]): Future[Seq[Int]] = Future.successful(s.map(_.length))
+
   def subtract(x: Int, y: Int = 1 + 1): String = s"$x-$y"
-  //def sum(p1: Point, p2: Point): Point = Point(p1.x + p2.x, p1.y + p2.y)
+
+//  def sum(p1: Point, p2: Point): Point = Point(p1.x + p2.x, p1.y + p2.y)
 }
 
-//case class Point(x: Int, y: Int)
+case class Point(x: Int, y: Int)
--- autowire/shared/src/test/scala/autowire/UpickleTests.scala
+++ autowire/shared/src/test/scala/autowire/UpickleTests.scala
@@ -1,46 +1,54 @@
 package autowire
+
 import utest._
-import utest.framework._
 import utest.framework.ExecutionContext.RunNow
-import ujson.Value._
-import upickle.default._
-import acyclic.file
 
 
-object UpickleTests extends TestSuite{
-  object Bundle extends GenericClientServerBundle[String, upickle.default.Reader, upickle.default.Writer]{
-    def write[T: upickle.default.Writer](t: T) = upickle.default.write(t)
-    def read[T: upickle.default.Reader](t: String) = upickle.default.read[T](t)
+object UpickleTests extends TestSuite {
+
+  object Bundle extends GenericClientServerBundle[String, upickle.default.Reader, upickle.default.Writer] {
+
+    def write[T: upickle.default.Writer](t: T) = {
+      upickle.default.write(t)
+    }
+
+    def read[T: upickle.default.Reader](t: String) = {
+      upickle.default.read[T](t)
+    }
+
     def routes = Server.route[Api](Controller)
   }
+
   import Bundle.{Client, Server}
 
   import utest.PlatformShims.await
 
-  val tests = utest.Tests{
-    test("example"){
+  val tests = utest.Tests {
+    test("example") {
       import upickle._
 
       // shared API interface
-      trait MyApi{
+      trait MyApi {
         def doThing(i: Int, s: String): Seq[String]
       }
 
       // server-side implementation, and router
-      object MyApiImpl extends MyApi{
+      object MyApiImpl extends MyApi {
         def doThing(i: Int, s: String) = Seq.fill(i)(s)
       }
-      object MyServer extends autowire.Server[String, upickle.default.Reader, upickle.default.Writer]{
-        def write[Result: Writer](r: Result) = upickle.default.write(r)
-        def read[Result: Reader](p: String) = upickle.default.read[Result](p)
+      object MyServer extends autowire.Server[String, upickle.default.Reader, upickle.default.Writer] {
+        def write[Result: upickle.default.Writer](r: Result) = upickle.default.write(r)
+
+        def read[Result: upickle.default.Reader](p: String) = upickle.default.read[Result](p)
 
         val routes = MyServer.route[MyApi](MyApiImpl)
       }
 
       // client-side implementation, and call-site
-      object MyClient extends autowire.Client[String, upickle.default.Reader, upickle.default.Writer]{
-        def write[Result: Writer](r: Result) = upickle.default.write(r)
-        def read[Result: Reader](p: String) = upickle.default.read[Result](p)
+      object MyClient extends autowire.Client[String, upickle.default.Reader, upickle.default.Writer] {
+        def write[Result: upickle.default.Writer](r: Result) = upickle.default.write(r)
+
+        def read[Result: upickle.default.Reader](p: String) = upickle.default.read[Result](p)
 
         override def doCall(req: Request) = {
           println(req)
@@ -50,7 +58,7 @@ object UpickleTests extends TestSuite{
 
       MyClient[MyApi].doThing(3, "lol").call().foreach(println)
     }
-    test("inheritedTraits"){
+    test("inheritedTraits") - {
       import upickle._
 
       // It should also be possible to separate the API into several controllers that
@@ -79,16 +87,18 @@ object UpickleTests extends TestSuite{
         with BookController
         with ArticleController
 
-      object MyServer extends autowire.Server[String, upickle.default.Reader, upickle.default.Writer]{
-        def write[Result: Writer](r: Result) = upickle.default.write(r)
-        def read[Result: Reader](p: String) = upickle.default.read[Result](p)
+      object MyServer extends autowire.Server[String, upickle.default.Reader, upickle.default.Writer] {
+        def write[Result: upickle.default.Writer](r: Result) = upickle.default.write(r)
+
+        def read[Result: upickle.default.Reader](p: String) = upickle.default.read[Result](p)
 
         val routes = MyServer.route[Protocol](Controller)
       }
 
-      object MyClient extends autowire.Client[String, upickle.default.Reader, upickle.default.Writer]{
-        def write[Result: Writer](r: Result) = upickle.default.write(r)
-        def read[Result: Reader](p: String) = upickle.default.read[Result](p)
+      object MyClient extends autowire.Client[String, upickle.default.Reader, upickle.default.Writer] {
+        def write[Result: upickle.default.Writer](r: Result) = upickle.default.write(r)
+
+        def read[Result: upickle.default.Reader](p: String) = upickle.default.read[Result](p)
 
         override def doCall(req: Request) = {
           println(req)
@@ -98,33 +108,36 @@ object UpickleTests extends TestSuite{
 
       MyClient[Protocol].bookList().call().foreach(println)
     }
-    test("basicCalls"){
+    test("basicCalls") - {
+
       val res1 = await(Client[Api].add(1, 2, 3).call())
       val res2 = await(Client[Api].add(1).call())
       val res3 = await(Client[Api].add(1, 2).call())
       val res4 = await(Client[Api].multiply(x = 1.2, Seq(2.3)).call())
       val res5 = await(Client[Api].multiply(x = 1.1, ys = Seq(2.2, 3.3, 4.4)).call())
-   //   val res6 = await(Client[Api].sum(Point(1, 2), Point(10, 20)).call())
+      //val res6 = await(Client[Api].sum(Point(1, 2), Point(10, 20)).call())
+
       assert(
         res1 == "1+2+3",
         res2 == "1+2+10",
         res3 == "1+2+10",
         res4 == "1.2*2.3",
         res5 == "1.1*2.2*3.3*4.4",
-     //   res6 == Point(11, 22)
+       // res6 == Point(11, 22)
       )
+
       Bundle.transmitted.last
     }
-    test("aliased"){
+    test("aliased") - {
       val api = Client[Api]
       val res = await(api.add(1, 2, 4).call())
       assert(res == "1+2+4")
     }
-    test("async"){
+    test("async") - {
       val res5 = await(Client[Api].sloww(Seq("omgomg", "wtf")).call())
       assert(res5 == Seq(6, 3))
     }
-    test("compilationFailures"){
+    test("compilationFailures") - {
 
       * - compileError("123.call()").check(
         """
@@ -151,15 +164,15 @@ object UpickleTests extends TestSuite{
         "value fail1 is not a member of autowire.ClientProxy"
       )
     }
-    test("runtimeFailures"){
-      test("noSuchRoute"){
+    test("runtimeFailures") - {
+      test("noSuchRoute") - {
         val badRequest = Core.Request[String](Seq("omg", "wtf", "bbq"), Map.empty)
         assert(!Server.routes.isDefinedAt(badRequest))
         intercept[MatchError] {
           Server.routes(badRequest)
         }
       }
-      test("inputError"){
+      test("inputError") {
         def check(input: Map[String, String])(expectedError: PartialFunction[Throwable, Unit]) = {
           val badRequest = Core.Request(
             Seq("autowire", "Api", "multiply"),
@@ -171,90 +184,115 @@ object UpickleTests extends TestSuite{
         }
 
         test("keysMissing") {
-          * - check(Map.empty){
-            case Error.InvalidInput(
+          test - {
+            check(Map.empty) {
+              case Error.InvalidInput(
               Error.Param.Missing("x"),
               Error.Param.Missing("ys")
-            ) =>
+              ) =>
+            }
           }
-          * - check(Map("x" -> "123")){
-            case Error.InvalidInput(Error.Param.Missing("ys")) =>
+          test - {
+            check(Map("x" -> "123")) {
+              case Error.InvalidInput(Error.Param.Missing("ys")) =>
+            }
           }
-          * - check(Map("ys" -> "[123]")){
-            case Error.InvalidInput(Error.Param.Missing("x")) =>
+          test - {
+            check(Map("ys" -> "[123]")) {
+              case Error.InvalidInput(Error.Param.Missing("x")) =>
+            }
           }
 
         }
         test("keysInvalid") - {
-          * - check(Map("x" -> "[]", "ys" -> "234")) {
-            case Error.InvalidInput(
-              Error.Param.Invalid("x", InvalidData(Arr(Seq()), _)),
-              Error.Param.Invalid("ys", InvalidData(Num(234), _))
-            ) =>
+          test - {
+            check(Map("x" -> "[]", "ys" -> "234")) {
+              case Error.InvalidInput(
+              Error.Param.Invalid("x", upickle.core.AbortException(_, _, _, _, _)),
+              Error.Param.Invalid("ys", upickle.core.AbortException(_, _, _, _, _))
+              )
+              =>
+            }
           }
-          * - check(Map("x" -> "123", "ys" -> "234")) {
-            case Error.InvalidInput(
-              Error.Param.Invalid("ys", InvalidData(Num(234), _))
-            ) =>
+          test - {
+            check(Map("x" -> "123", "ys" -> "234")) {
+              case Error.InvalidInput(
+              Error.Param.Invalid("ys", upickle.core.AbortException(_, _, _, _, _))
+              ) =>
+            }
           }
-          * - check(Map("x" -> "[]", "ys" -> "[234]")) {
-            case Error.InvalidInput(
-              Error.Param.Invalid("x", InvalidData(Arr(Seq()), _))
-            ) =>
+          test - {
+            check(Map("x" -> "[]", "ys" -> "[234]")) {
+              case Error.InvalidInput(
+              Error.Param.Invalid("x", upickle.core.AbortException(_, _, _, _, _))
+              ) =>
+            }
           }
         }
 
         test("invalidJson") - {
-          * - check(Map("x" -> "]", "ys" -> "2}34")) {
-            case Error.InvalidInput(
-              Error.Param.Invalid("x", InvalidData(_, "]")),
-              Error.Param.Invalid("ys", InvalidData(_, "2}34"))
-            ) =>
+          test - {
+            check(Map("x" -> "]", "ys" -> "2}34")) {
+              case Error.InvalidInput(
+              Error.Param.Invalid("x", ujson.ParseException(_, _, _, _)),
+              Error.Param.Invalid("ys", upickle.core.AbortException(_, _, _, _, _))
+              ) =>
+            }
           }
-          * - check(Map("x" -> "1", "ys" -> "2}34")) {
-            case Error.InvalidInput(
-              Error.Param.Invalid("ys", InvalidData(_, "2}34"))
-            ) =>
+          test - {
+            check(Map("x" -> "1", "ys" -> "2}34")) {
+              case Error.InvalidInput(
+              Error.Param.Invalid("ys", upickle.core.AbortException(_, _, _, _, _))
+              ) =>
+            }
           }
-          * - check(Map("x" -> "]", "ys" -> "[234]")) {
-            case Error.InvalidInput(
-              Error.Param.Invalid("x", InvalidData(_, "]"))
-            ) =>
+          test - {
+            check(Map("x" -> "]", "ys" -> "[234]")) {
+              case Error.InvalidInput(
+              Error.Param.Invalid("x", ujson.ParseException(_, _, _, _))
+              ) =>
+            }
           }
         }
 
         test("mix") - {
-          * - check(Map("x" -> "]")) {
-            case Error.InvalidInput(
-              Error.Param.Invalid("x", InvalidData(_, "]")),
+          test - {
+            check(Map("x" -> "]")) {
+              case Error.InvalidInput(
+              Error.Param.Invalid("x", ujson.ParseException(_, _, _, _)),
               Error.Param.Missing("ys")
-            ) =>
+              ) =>
+            }
           }
-          * - check(Map("x" -> "[1]", "ys" -> "2}34")) {
-            case Error.InvalidInput(
-              Error.Param.Invalid("x", InvalidData(Arr(Seq(Num(1))), _)),
-              Error.Param.Invalid("ys", InvalidData(_, "2}34"))
-            ) =>
-          }
+          test -
+            check(Map("x" -> "[1]", "ys" -> "2}34")) {
+              case Error.InvalidInput(
+              Error.Param.Invalid("x", upickle.core.AbortException(_, _, _, _, _)),
+              Error.Param.Invalid("ys", upickle.core.AbortException(_, _, _, _, _))
+              ) =>
+            }
         }
       }
     }
     test("classImpl") - {
       // Make sure you can pass things other than `object`s (e.g. instances)
       // to the autowire router, and that it still works
-      trait MyApi{
+      trait MyApi {
         def doThing(i: Int, s: String): Seq[String]
+
         def doThingTwo(i: Int, s: String = "A"): Seq[String]
       }
 
-      class MyOtherApiImpl(meaningOfLife: Int) extends MyApi{
-        def doThing(i: Int, s: String) = Seq.fill(i)(s+meaningOfLife.toString)
-        def doThingTwo(i: Int, s: String) = Seq.fill(i)(s+meaningOfLife.toString)
+      class MyOtherApiImpl(meaningOfLife: Int) extends MyApi {
+        def doThing(i: Int, s: String) = Seq.fill(i)(s + meaningOfLife.toString)
+
+        def doThingTwo(i: Int, s: String) = Seq.fill(i)(s + meaningOfLife.toString)
       }
 
-      object MyServer extends autowire.Server[String, upickle.default.Reader, upickle.default.Writer]{
-        def write[Result: Writer](r: Result) = upickle.default.write(r)
-        def read[Result: Reader](p: String) = upickle.default.read[Result](p)
+      object MyServer extends autowire.Server[String, upickle.default.Reader, upickle.default.Writer] {
+        def write[Result: upickle.default.Writer](r: Result) = upickle.default.write(r)
+
+        def read[Result: upickle.default.Reader](p: String) = upickle.default.read[Result](p)
 
         val routes1 = MyServer.route[MyApi](new MyOtherApiImpl(42))
 
@@ -262,11 +300,14 @@ object UpickleTests extends TestSuite{
         val routes2 = MyServer.route[MyApi](anApi)
 
         def anApiDef(inp: Int) = new MyOtherApiImpl(inp)
+
         val routes3 = MyServer.route[MyApi](anApiDef(2))
       }
-      class UpickleClient(pf: PartialFunction[MyServer.Request, concurrent.Future[String]]) extends autowire.Client[String, upickle.default.Reader, upickle.default.Writer]{
-        def write[Result: Writer](r: Result) = upickle.default.write(r)
-        def read[Result: Reader](p: String) = upickle.default.read[Result](p)
+      class UpickleClient(pf: PartialFunction[MyServer.Request, concurrent.Future[String]]) extends autowire.Client[String, upickle.default.Reader, upickle.default.Writer] {
+        def write[Result: upickle.default.Writer](r: Result) = upickle.default.write(r)
+
+        def read[Result: upickle.default.Reader](p: String) = upickle.default.read[Result](p)
+
         def doCall(req: Request) = pf(req)
       }
       object Client1 extends UpickleClient(MyServer.routes1)
@@ -275,14 +316,14 @@ object UpickleTests extends TestSuite{
       object Client3 extends UpickleClient(MyServer.routes3)
 
       val res1 = await(Client1[MyApi].doThingTwo(3).call())
-      val res2 = await(Client1[MyApi].doThingTwo(3,"B").call())
+      val res2 = await(Client1[MyApi].doThingTwo(3, "B").call())
       val res3 = await(Client2[MyApi].doThingTwo(2).call())
-      val res4 = await(Client3[MyApi].doThingTwo(3,"C").call())
+      val res4 = await(Client3[MyApi].doThingTwo(3, "C").call())
       assert(
-        res1 == List("A42","A42","A42"),
-        res2 == List("B42","B42","B42"),
-        res3 == List("A1","A1"),
-        res4 == List("C2","C2","C2")
+        res1 == List("A42", "A42", "A42"),
+        res2 == List("B42", "B42", "B42"),
+        res3 == List("A1", "A1"),
+        res4 == List("C2", "C2", "C2")
       )
     }
   }
